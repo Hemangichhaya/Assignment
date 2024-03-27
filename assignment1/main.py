86179f7988262fd65a274a8918e1b4acdccbfbe2
Hemangichhaya <72784802+Hemangichhaya@users.noreply.github.com>
2024-03-27 08:47:54 +0530

    Updated

--- assignment1/main.py
+++ assignment1/main.py
@@ -6,14 +6,14 @@ from evaluate_func import *
 
 if __name__ == '__main__':
     ##split lines by \n
-    # data('./data/mod_tain.csv', 'train_new', './data/train')
+    # data('./data/mod_train.csv', 'train_new', './data/train')
     # data('./data/test.csv', 'test_new', './data/test')
     # train_spacy_model_('./data/train_new_spacy.json', './data/test_new_spacy.json', './models/split_lines_main')
     # model_eval('./models/split_lines_main', './data/test_new_spacy.json')
     model_eval('./models/split_lines_main', './data/train_new_spacy.json')
 
     # split lines with fixed len 500 characters
-    # data('./data/mod_tain.csv', 'train_fixedlen', './data/train')
+    # data('./data/mod_train.csv', 'train_fixedlen', './data/train')
     # data('./data/test.csv', 'test_fixedlen', './data/test')
     # train_spacy_model_('./data/train_fixedlen_spacy.json', './data/test_fixedlen_spacy.json', './models/split_fixedlen_main')
     # model_eval('./models/split_fixedlen_main', './data/test_fixedlen_spacy.json')
